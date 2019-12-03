0f95a59cba5240db6fba78ce9e936481598dd447
zebnitskyMax <58050667+zebnitskyMax@users.noreply.github.com>
2019-12-04 01:05:54 +0300

    add new project Calculator

---
+++ Calculator/js/javascript.js
@@ -0,0 +1,16 @@
+/* function contant() {
+    var val = document.getElementsByClassName('button').value;
+    document.getElementsByClassName('text').innerHTML = val;
+} */
+function sign(num) {
+    var divide = document.getElementById('divide').value;
+    var text = document.getElementById('text');
+    text.innerHTML = divide;
+    var something;
+    document.getElementById('11') = something;
+    something.innerHTML = "fuck"
+
+}
+var a = 3;
+console.log(a);
+
